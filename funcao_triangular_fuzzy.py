1412e170e7cd92d9e058d392b701d1200c9b8594
ERNANI DE SOUZA CUBAS NETO <escneto@ad.mppr.mp.br>
2023-03-03 15:25:57 -0300

    funcao triangular fuzzy v2

--- funcao_triangular_fuzzy.py
+++ funcao_triangular_fuzzy.py
@@ -16,10 +16,17 @@ def funcao_triangular(d):
 
   return grau_pertinencia
 
-d = 10
+d = int(input("Digite o valor de d:"))
 coordenada_x = list(range((2*d)+1))
 coordenada_y = funcao_triangular(d)
 
-plt.plot(coordenada_x,coordenada_y)
-plt.axvline(x = d,color = 'red')
+fig, ax = plt.subplots(figsize=(6, 3))
+ax.set(xlim=(-1, (2*d)+1), ylim=(0, 1))
+ax.scatter(x=0,y=0)
+ax.scatter(x=d,y=0,label="b")
+ax.scatter(x=d*2,y=0,label="c")
+
+ax.plot(coordenada_x,coordenada_y)
+
+ax.axvline(x = d,color = 'red', ymin=0,ymax=1)
 plt.show()
